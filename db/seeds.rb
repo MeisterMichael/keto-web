22722dfe6494b64a6666a87e54da93ef21e862ca
Michael Ferguson <meister@spacekace.com>
2018-09-30 17:39:21 -0700

    admin seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+helpers = ActionController::Base.helpers
+
+default_password = ENV['DEFAULT_PASSWORD'] || '1234'
+
+User.destroy_all
+admin_user = User.create( username: 'meister', email: 'meister@spacekace.com', first_name: 'Michael', last_name: 'Ferguson', status: 'active', role: 'admin', password: default_password )
+admin_user = User.create( username: 'Gk', email: 'gk@gk.com', first_name: 'Gk', last_name: 'PP', status: 'active', role: 'admin', password: default_password )
