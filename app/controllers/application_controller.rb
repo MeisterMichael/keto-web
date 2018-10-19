6539afcb67fc84025c594b6f473f23eb5cdcbf27
Michael Ferguson <meister@spacekace.com>
2018-10-19 16:36:07 -0700

    addthis - stash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
 	include Pulitzer::Concerns::ApplicationControllerConcern
+	include Devise::Controllers::Helpers
 
+	before_action :register_then
 	before_action :set_page_meta
 
 
@@ -26,9 +28,39 @@ class ApplicationController < ActionController::Base
 		def client_ip_country
 			request.headers['CF-IPCountry']
 		end
-		
+
 		def log_event( opts={} )
 			true
+		end
+
+		def register_then
+
+			if params[:register_then].present?
+				user_params = params.require(:register_then).permit( :email, :full_name, :username )
+
+				user = User.where( email: user_params[:email] ).first
+				user ||= User.new( user_params.merge(ip: request.ip) )
+
+				if user.encrypted_password.present?
+					# this email is already registered for this site
+					set_flash "#{user.email} is already registered. Try <a href='/forgot'>Forgot Password</a>.", :error
+					redirect_back( fallback_location: '/register' )
+					return false
+				end
+
+				user.password = params[:register_then].require(:password)
+
+				if user.save
+					sign_in( :user, user )
+				else
+					set_flash "Could not register user.", :error, user
+					redirect_back( fallback_location: '/register' )
+					return false
+				end
+
+				log_event( { name: 'register', content: 'registered.' } )
+
+			end
 		end
 
 
