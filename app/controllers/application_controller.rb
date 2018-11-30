aed377f50978b4b84d07a4a951660e5ae4c4412a
Michael Ferguson <meister@spacekace.com>
2018-11-29 16:44:20 -0800

    email to admin self

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,7 +43,7 @@ class ApplicationController < ActionController::Base
 			end
 
 			if %w(add_topic).include? opts[:name]
-				listening_admins = User.admin.where( username: (ENV['LISTENING_ADMINS_USERNAMES'] || '').split(/\s+/).collect(&:strip) ).where.not( id: current_user.id )
+				listening_admins = User.admin.where( username: (ENV['LISTENING_ADMINS_USERNAMES'] || '').split(/\s+/).collect(&:strip) ) #.where.not( id: current_user.id )
 				posters_subscriptions = Scuttlebutt::Subscription.where( parent_obj: current_user ).where.not( user: listening_admins ).where.not( user: current_user )
 
 				target_obj = opts[:on] || opts[:target_obj]
