5198aa6cda5993e9b0ea0dc72293ab1e6ac27b33
Michael Ferguson <meister@spacekace.com>
2019-01-10 15:31:08 -0800

    full text search for discussion topic

--- app/models/concerns/scuttlebutt/discussion_topic_searchable.rb
+++ app/models/concerns/scuttlebutt/discussion_topic_searchable.rb
@@ -10,9 +10,11 @@ module Scuttlebutt
 					indexes :id, type: 'integer'
 					indexes :created_at, type: 'date'
 					indexes :subject, analyzer: 'english', index_options: 'offsets'
+					indexes :content, analyzer: 'english', index_options: 'offsets'
 					indexes :rating, type: 'integer'
 					indexes :status
 					indexes :public, type: 'boolean'
+					indexes :full_text, analyzer: 'english', index_options: 'offsets'
 				end
 			end
 		end
@@ -24,7 +26,10 @@ module Scuttlebutt
 		# Instance Methods
 		# def instance_method_name ... end
 		def as_indexed_json(options={})
-			as_json().merge( 'public' => (self.anyone? && self.active?) )
+			full_text = self.content
+			full_text = "#{full_text}\n#{self.posts.collect{|post| post.content }.join("\n")}"
+
+			as_json().merge( 'public' => (self.anyone? && self.active?), full_text: full_text )
 		end
 
 	end
