733f349a7ea4dc9e3ea986324a98a60c3f64a6f2
Michael Ferguson <meister@spacekace.com>
2019-01-20 11:25:04 -0800

    allow br tags

--- config/initializers/scuttlebutt.rb
+++ config/initializers/scuttlebutt.rb
@@ -2,7 +2,7 @@ Scuttlebutt.configure do |config|
 
 	# config.discussion_path = 'discussions'
 	config.discussion_topic_path = 're'
-	# config.post_allowed_tags = %w( strong table tr td thead tbody i em p div a img alt )
+	config.post_allowed_tags = %w( strong table tr td thead tbody i em p div a img alt br )
 	# config.post_allowed_attributes = %w( class href src style )
 
 end
