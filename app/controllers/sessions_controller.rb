d465cd25c771cbac1343aea0e1ecb4ded099c5a8
Michael Ferguson <meister@spacekace.com>
2019-02-27 08:43:50 -0800

    fix password reset

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < Devise::SessionsController
-	
+
+	layout 'sessions'
 
 	def create
 		self.resource = warden.authenticate!( auth_options )
-		
+
 		sign_in( resource_name, resource )
-		
+
 		redirect_to after_sign_in_path_for( resource )
 
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -31,12 +31,12 @@ Rails.application.routes.draw do
 	resources :usda_food_admin
 
 	devise_scope :user do
+		get '/forgot' => 'passwords#new', as: 'forgot'
 		get '/login' => 'sessions#new', as: 'login'
 		get '/logout' => 'sessions#destroy', as: 'logout'
 		get '/register' => 'registrations#new', as: 'register'
 	end
-	devise_for :users, :controllers => { :registrations => 'registrations', :sessions => 'sessions' }
-
+	devise_for :users, :controllers => { :registrations => 'registrations', :sessions => 'sessions', :passwords => 'passwords' }
 
 	resources :users, path: User.mounted_path
 
